956f11a3cd94d9a0c5adf57b28d57537108ec492
Lin Zhang <linzhank@gmail.com>
2023-06-30 16:23:45 -0500

    update replay buffer

--- agents/dqn.py
+++ agents/dqn.py
@@ -1,11 +1,52 @@
 """Class and functions to implement a simple DQN agent"""
 
-import collections
+from collections import namedtuple
 import flax.linen as nn
 import jax
 import jax.numpy as jnp
 import optax
 from distrax import Greedy, EpsilonGreedy
+
+
+Batch = namedtuple('Batch', ['pobs', 'acts', 'rews', 'termsigs', 'nobs'])
+Params = namedtuple('Params', 'online, target')
+
+
+class ReplayBuffer(object):
+    """A simple off-policy replay buffer."""
+
+    def __init__(self, capacity, dim_obs):
+        self.pobs_stash = jnp.zeros(shape=[capacity]+list(dim_obs), dtype=jnp.float32)
+        self.acts_stash = jnp.zeros(shape=capacity, dtype=jnp.float32)
+        self.rews_stash = jnp.zeros(shape=capacity, dtype=jnp.float32)
+        self.nobs_stash = jnp.zeros_like(self.pobs_stash)
+        self.termsigs_stash = jnp.zeros(shape=capacity, dtype=jnp.float32)
+        self.loc = 0  # replay instance index
+        self.stash_size = 0
+        self.capacity = capacity
+
+    def store(self, prev_obs, action, reward, term_signal, next_obs):
+        self.pobs_stash[self.loc] = prev_obs
+        self.acts_stash[self.loc] = action
+        self.rews_stash[self.loc] = reward
+        self.nobs_stash[self.loc] = next_obs
+        self.termsigs_stash[self.loc] = term_signal
+        self.loc = (self.loc + 1) % self.capacity
+        self.stash_size = min(self.stash_size + 1, self.capacity)
+
+    def sample(self, key, batch_size, discount_factor=0.99):
+        key, subkey = jax.random.split(key)
+        indices = jax.random.randint(subkey, shape=(batch_size,), minval=0, maxval=self.stash_size)
+        pobs_samples = self.pobs_stash[indices]
+        nobs_samples = self.nobs_stash[indices]
+        acts_samples = self.acts_stash[indices]
+        rews_samples = self.rews_stash[indices]
+        termsigs_samples = self.termsigs_stash[indices]
+        batched_samples = Batch(pobs_samples, acts_samples, rews_samples, termsigs_samples, nobs_samples)
+        return batched_samples
+
+    def is_ready(self, batch_size):  # warm up trick
+        return batch_size <= self.capacity
 
 
 class QNet(nn.Module):
@@ -28,9 +69,6 @@ class QNet(nn.Module):
         return logits
 
 
-Params = collections.namedtuple('Params', 'online, target')
-
-
 class DQNAgent:
     """DQN agent template"""
 
@@ -47,9 +85,9 @@ class DQNAgent:
 
     def init_params(self, key, sample_obs):
         online_params = self.qnet.init(key, sample_obs)
-        self.params_container = Params(online_params, online_params)
+        params = Params(online_params, online_params)
 
-        return self.params_container
+        return params
 
     def make_decision(self, key, params, obs, episode_count, eval_flag=False):
         """pi(a|s)
@@ -74,8 +112,9 @@ if __name__=='__main__':
         observation_shape=env.observation_space.shape,
         num_actions=env.action_space.n
     )
+    buf = ReplayBuffer(capacity=int(10), dim_obs=env.observation_space.shape)
     key = jax.random.PRNGKey(20)
-    params = agent.init_params(
+    qnet_params = agent.init_params(
         key=key,
         sample_obs=env.observation_space.sample()
     )
@@ -83,14 +122,15 @@ if __name__=='__main__':
     o_tm1, info = env.reset()
     term, trunc = False, False
     episode_count = 0
-    for step in range(1000):
+    for step in range(200):
         key, a_tm1, qvals, epsilon = agent.make_decision(
             key=key,
-            params=agent.params_container.online,
+            params=qnet_params.online,
             obs=o_tm1,
             episode_count=episode_count,
         )
         o_t, r_t, term, trunc, info = env.step(int(a_tm1))
+        buf.store(prev_obs=o_tm1, action=a_tm1, reward=r_t, terminated=term, next_obs=o_t)
         print(f"\n---episode: {episode_count}, step: {step}, epsilon: {epsilon}---")
         print(f"state: {o_tm1}\naction: {a_tm1}\nreward: {r_t}\nterminated? {term}\ntruncated? {trunc}\ninfo: {info}\nnext state: {o_t}")
         o_tm1 = o_t
