d07e45a535fdec7e4568fb8a3094b7dfed1aeac2
Lin Zhang <linzhank@gmail.com>
2023-08-15 10:55:26 -0500

    add cartpole test

--- agents/dqn.py
+++ agents/dqn.py
@@ -71,9 +71,9 @@ class QNet(nn.Module):
         Network is used to estimate values of state-action pairs
         """
         x = inputs.astype(jnp.float32)
-        x = nn.Dense(features=128, name='dense1')(x)
+        x = nn.Dense(features=256, name='dense1')(x)
         x = nn.relu(x)
-        x = nn.Dense(features=128, name='dense2')(x)
+        x = nn.Dense(features=256, name='dense2')(x)
         x = nn.relu(x)
         logits = nn.Dense(features=self.num_outputs, name='logits')(x)
         return logits
@@ -84,15 +84,13 @@ class DQNAgent:
 
     def __init__(
         self,
-        observation_shape: tuple,
-        num_actions: int,
-        key: int = 123,
-        epsilon_transition_episodes: int = 500,
-        learning_rate: float = 1e-4,
-        target_update_period: int = 100,
+        obs_shape,
+        num_actions,
+        epsilon_transition_episodes=700,
+        learning_rate=2e-4,
+        target_update_period=100,
+        polyak_step_size=0.003,
     ):
-        self.observation_shape = observation_shape
-        self.num_actions = num_actions
         self.qnet = QNet(num_outputs=num_actions)
         self.epsilon_by_frame = optax.polynomial_schedule(
             init_value=1.0,
@@ -104,54 +102,64 @@ class DQNAgent:
         # variables
         self.update_count = 0
         # properties
-        self.key = jax.random.PRNGKey(key)
+        self.obs_shape = obs_shape
+        self.num_actions = num_actions
         self.update_period = target_update_period
+        self.polyak_step_size = polyak_step_size
         # jit for speed
         self.make_decision = jax.jit(self.make_decision)
         self.update_params = jax.jit(self.update_params)
 
-    def init_params(self, sample_obs):
-        online_params = self.qnet.init(self.key, sample_obs)
+    def init_params(self, key, sample_obs):
+        online_params = self.qnet.init(key, sample_obs)
         params = Params(online_params, online_params)
 
         return params
 
     def init_optmizer(self, init_params):
-        self.opt_state = self.optimizer.init(init_params)
+        opt_state = self.optimizer.init(init_params)
 
-    def make_decision(self, params, obs, episode_count, eval_flag=False):
+        return opt_state
+
+    def make_decision(self, key, params, obs, episode_count, eval_flag=False):
         """pi(a|s)
         TODO:
             add warm up
         """
-        self.key, subkey = jax.random.split(self.key)  # generate a new key, or sampled action won't change
+        key, subkey = jax.random.split(key)  # generate a new key, or sampled action won't change
         qvals = jnp.squeeze(self.qnet.apply(params, obs))
         epsilon = self.epsilon_by_frame(episode_count)
         sampled_action = EpsilonGreedy(preferences=qvals, epsilon=epsilon).sample(seed=subkey)
         greedy_action = Greedy(preferences=qvals).sample(seed=subkey)
         action = jax.lax.select(eval_flag, greedy_action, sampled_action)
 
-        return action, qvals, epsilon
+        return key, action, qvals, epsilon
 
-    def update_params(self, params, replay_batch):
+    def update_params(self, params, replay_batch, opt_state):
         """Periodic update online params.
 
-        TODO: add polyak update
         """
-        target_params = optax.periodic_update(
-            params.online,
-            params.target,
-            self.update_count,
-            self.update_period,
-        )
+        if self.polyak_step_size > 0 and self.polyak_step_size < 1:
+            target_params = optax.incremental_update(
+                params.online,
+                params.target,
+                self.polyak_step_size,
+            )
+        else:
+            target_params = optax.periodic_update(
+                params.online,
+                params.target,
+                self.update_count,
+                self.update_period,
+            )
         loss_val, grads = jax.value_and_grad(self.loss_fn)(
             params.online, params.target, replay_batch
         )
-        updates, self.opt_state = self.optimizer.update(grads, self.opt_state)
+        updates, opt_state = self.optimizer.update(grads, opt_state)
         online_params = optax.apply_updates(params.online, updates)
         self.update_count += 1
 
-        return loss_val, Params(online_params, target_params)
+        return loss_val, Params(online_params, target_params), opt_state
 
     def loss_fn(
         self,
@@ -190,57 +198,63 @@ if __name__ == '__main__':
     # setup env, agent and replay buffer
     env = gym.make('LunarLander-v2')  # , render_mode='human')
     agent = DQNAgent(
-        observation_shape=env.observation_space.shape,
+        obs_shape=env.observation_space.shape,
         num_actions=env.action_space.n,
-        key=0,
     )
     buf = ReplayBuffer(capacity=int(1e6), dim_obs=env.observation_space.shape)
-    params = agent.init_params(sample_obs=env.observation_space.sample())
-    agent.init_optmizer(params.online)
+    # init params and optimizer
+    key = jax.random.PRNGKey(123)
+    qnet_params = agent.init_params(key, env.observation_space.sample())
+    opt_state = agent.init_optmizer(qnet_params.online)
     # init env
-    o_tm1, info = env.reset()
+    pobs, _ = env.reset()
     term, trunc = False, False
     episode_count, episodic_return = 0, 0
     deposit_return, averaged_return = [], []
-    for step in range(int(3e5)):
-        a_tm1, qvals, epsilon = agent.make_decision(
-            params=params.online,
-            obs=o_tm1,
+    for step in range(int(4e5)):
+        key, act, qvals, epsilon = agent.make_decision(
+            key=key,
+            params=qnet_params.online,
+            obs=pobs,
             episode_count=episode_count,
         )
-        o_t, r_t, term, trunc, info = env.step(int(a_tm1))
-        buf.store(prev_obs=o_tm1, action=a_tm1, reward=r_t, term_signal=term, next_obs=o_t)
-        episodic_return += r_t
+        nobs, rew, term, trunc, info = env.step(int(act))
+        buf.store(prev_obs=pobs, action=act, reward=rew, term_signal=term, next_obs=nobs)
+        episodic_return += rew
         # print(f"\n---episode: {episode_count}, step: {step}, return: {episodic_return}---")
-        # print(f"state: {o_tm1}\naction: {a_tm1}\nreward: {r_t}\nterminated? {term}\ntruncated? {trunc}\ninfo: {info}\nnext state: {o_t}")
-        o_tm1 = o_t
-        if buf.buffer_size > 2048:
-            batch_loss, params = agent.update_params(params, buf.sample(batch_size=2048))
+        # print(f"state: {pobs}\naction: {act}\nreward: {rew}\nterminated? {term}\ntruncated? {trunc}\ninfo: {info}\nnext state: {nobs}")
+        pobs = nobs
+        if buf.buffer_size > 1024:
+            batch_loss, qnet_params, opt_state = agent.update_params(qnet_params, buf.sample(batch_size=1024), opt_state)
             # print(f"loss: {batch_loss}")
         if term or trunc:
             episode_count += 1
             deposit_return.append(episodic_return)
             averaged_return.append(sum(deposit_return) / episode_count)
             print(f"\n---episode: {episode_count}, steps: {step}, epsilon: {epsilon}, average return: {averaged_return[-1]}---\n")
-            o_tm1, info = env.reset()
+            pobs, _ = env.reset()
             term, trunc = False, False
             episodic_return = 0
             print("reset")
-    plt.plot(averaged_return)
-    plt.show()
     # validation
     env = gym.make('LunarLander-v2', render_mode='human')
-    o_tm1, info = env.reset()
+    pobs, _ = env.reset()
     term, trunc = False, False
     for _ in range(1000):
-        a_tm1, qvals, epsilon = agent.make_decision(
-            params=params.online,
-            obs=o_tm1,
+        key, act, qvals, epsilon = agent.make_decision(
+            key=key,
+            params=qnet_params.online,
+            obs=pobs,
             episode_count=episode_count,
             eval_flag=True,
         )
-        o_t, r_t, term, trunc, info = env.step(int(a_tm1))
+        nobs, rew, term, trunc, info = env.step(int(act))
+        print(f"state: {pobs}\naction: {act}\nreward: {rew}\nterminated? {term}\ntruncated? {trunc}\ninfo: {info}\nnext state: {nobs}")
+        pobs = nobs
         if term or trunc:
             break
+    env.close()
+    plt.plot(averaged_return)
+    plt.show()
 
 
