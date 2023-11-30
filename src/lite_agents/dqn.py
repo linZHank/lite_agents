5516c64aacc37ae17708d4f1b6b2a82c15f438d5
linzhanguca <lzhang12@uca.edu>
2023-11-29 23:45:08 -0600

    create MLP

--- src/lite_agents/dqn.py
+++ src/lite_agents/dqn.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 import numpy as np
 import jax.numpy as jnp
+import flax.linen as nn
 
 
 Batch = namedtuple('Batch', ['pobs', 'acts', 'discrews', 'nobs'])
@@ -51,4 +52,30 @@ class ReplayBuffer(object):
     def is_ready(self, batch_size):  # warm up trick
         return batch_size <= self.capacity
 
+
+class MLP(nn.Module):
+    "Q-Net template"
+
+    num_outputs: int
+    hidden_sizes: list
+
+    @nn.compact
+    def __call__(self, inputs):
+        """Define the basic MLP network architecture
+
+        Network is used to estimate values of state-action pairs
+        """
+        # x = inputs.astype(jnp.float32)
+        # x = nn.Dense(features=self.layer_size, name='dense1')(x)
+        # x = nn.relu(x)
+        # x = nn.Dense(features=self.layer_size, name='dense2')(x)
+        # x = nn.relu(x)
+        # logits = nn.Dense(features=self.num_outputs, name='logits')(x)
+        dtype = jnp.float32
+        x = inputs.astype(dtype)
+        for i, size in enumerate(self.hidden_sizes):
+            x = nn.Dense(features=size, name='hidden'+str(i+1), dtype=dtype)(x)
+            x = nn.relu(x)
+        logits = nn.Dense(features=self.num_outputs, name='logits')(x)
+        return logits
 
---
+++ tests/mlp.py
@@ -0,0 +1,12 @@
+from lite_agents.dqn import MLP
+import jax.numpy as jnp
+import jax
+
+
+key = jax.random.PRNGKey(19)
+model = MLP(num_outputs=2, hidden_sizes=[4, 3])
+dummy_inputs = jax.random.normal(key, shape=(5, 7))
+print(dummy_inputs)
+dummy_params = model.init(key, dummy_inputs)
+print(dummy_params)
+print(model.apply(dummy_params, dummy_inputs))
