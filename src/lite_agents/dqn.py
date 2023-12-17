4935e302b38052e3b511f6d01fc861410c52d883
Lin Zhang <linzhank@gmail.com>
2023-12-17 08:56:59 -0600

    update disconted reward computation

--- src/lite_agents/dqn.py
+++ src/lite_agents/dqn.py
@@ -8,7 +8,7 @@ import optax
 from distrax import Greedy, EpsilonGreedy
 
 
-Batch = namedtuple('Batch', ['pobs', 'acts', 'discrews', 'nobs'])
+Batch = namedtuple('Batch', ['pob', 'act', 'nob', 'rew', 'term'])
 
 
 class ReplayBuffer(object):
@@ -20,37 +20,36 @@ class ReplayBuffer(object):
         self.buf_rews = np.zeros(shape=capacity, dtype=np.float32)
         self.buf_nobs = np.zeros_like(self.buf_pobs)
         self.buf_terms = np.zeros(shape=capacity, dtype=np.float32)
-        self.Batch = namedtuple('Batch', 'pobs, acts, drews, nobs')
         # variables
         self.loc = 0  # replay instance index
         self.buffer_size = 0
         # property
         self.capacity = capacity
 
-    def store(self, prev_obs, action, reward, next_obs, term_flag):
+    def store(self, prev_obs, action, next_obs, reward, term_flag):
         self.buf_pobs[self.loc] = prev_obs
         self.buf_acts[self.loc] = action
-        self.buf_rews[self.loc] = reward
         self.buf_nobs[self.loc] = next_obs
+        self.buf_rews[self.loc] = reward
         self.buf_terms[self.loc] = term_flag
         self.loc = (self.loc + 1) % self.capacity
         self.buffer_size = min(self.buffer_size + 1, self.capacity)
 
     def sample(self, batch_size, discount_factor=0.99):
         ids = np.random.randint(low=0, high=self.buffer_size, size=(batch_size,))
-        samples_pobs = jnp.array(self.buf_pobs[ids])
-        samples_acts = jnp.array(self.buf_acts[ids])
-        samples_rews = jnp.array(self.buf_rews[ids])
-        samples_nobs = jnp.array(self.buf_nobs[ids])
-        samples_terms = jnp.array(self.buf_terms[ids])
-        samples_drews = samples_rews * (1 - samples_terms) * discount_factor
-        sampled_batch = self.Batch(
-            samples_pobs,
-            samples_acts,
-            # rews_samples,
-            # termsigs_samples,
-            samples_drews,  # discounted rewards
-            samples_nobs
+        self.sample_ids = ids
+        sampled_pobs = jnp.array(self.buf_pobs[ids])
+        sampled_acts = jnp.array(self.buf_acts[ids])
+        sampled_nobs = jnp.array(self.buf_nobs[ids])
+        sampled_rews = jnp.array(self.buf_rews[ids])
+        sampled_terms = jnp.array(self.buf_terms[ids])
+        # sampled_drews = sampled_rews * (1 - sampled_terms) * discount_factor  # BIG MISTAKE HERE
+        sampled_batch = Batch(
+            sampled_pobs,
+            sampled_acts,
+            sampled_nobs,
+            sampled_rews,  # discounted rewards
+            sampled_terms,  # terminal flags
         )
         return sampled_batch
 
@@ -72,11 +71,11 @@ class MLP(nn.Module):
 
         """
         dtype = jnp.float32
-        X = inputs.astype(dtype)
+        x = inputs.astype(dtype)
         for i, size in enumerate(self.hidden_sizes):
-            Z = nn.Dense(features=size, name='hidden'+str(i+1), dtype=dtype)(X)
-            X = nn.relu(Z)
-        logits = nn.Dense(features=self.num_outputs, name='logits')(X)
+            z = nn.Dense(features=size, name='hidden'+str(i+1), dtype=dtype)(x)
+            x = nn.relu(z)
+        logits = nn.Dense(features=self.num_outputs, name='logits')(x)
         return logits
 
 
@@ -112,12 +111,12 @@ class DQNAgent:
         )
 
         # Jitted methods
-        self.value_fn = jax.jit(self.state.apply_fn)
+        self.qvalue_fn = jax.jit(self.state.apply_fn)
 
     def make_decision(self, obs, episode_count, eval_flag=True):
         self.key, subkey = jax.random.split(self.key)
         epsilon = self.epsilon_schedule(episode_count)
-        qvals = self.value_fn({'params': self.params_online}, obs).squeeze(axis=0)
+        qvals = self.qvalue_fn({'params': self.params_online}, obs).squeeze(axis=0)
         act_greedy = Greedy(preferences=qvals).sample(seed=subkey)
         act_sample = EpsilonGreedy(
             preferences=qvals,
@@ -133,13 +132,14 @@ class DQNAgent:
 
 if __name__ == '__main__':
     import gymnasium as gym
-    # Create env, agent
+    # SETUP
     env = gym.make('CartPole-v1', render_mode='human')
+    buffer = ReplayBuffer(capacity=100, obs_shape=env.observation_space.shape)
     agent = DQNAgent(
-        0,
-        env.observation_space.shape,
-        env.action_space.n,
-        (5, 3),
+        seed=0,
+        obs_shape=env.observation_space.shape,
+        num_actions=env.action_space.n,
+        hidden_sizes=(5, 3),
     )
     print(
         agent.qnet.tabulate(
@@ -148,13 +148,16 @@ if __name__ == '__main__':
             compute_flops=True,
             compute_vjp_flops=True
         )
-    )  # log network details
-    # Train agent
-    o, i = env.reset()
-    for _ in range(1000):
-        a = agent.make_decision(jnp.expand_dims(o, axis=0), 1, eval_flag=False)
-        print(a)
-        o, r, t, tr, i = env.step(int(a))
+    )  # view QNet structure in a table
+
+    # LOOP
+    o_0, i = env.reset()
+    for _ in range(100):
+        a = agent.make_decision(jnp.expand_dims(o_0, axis=0), 1, eval_flag=False)
+        # print(a)
+        o_1, r, t, tr, i = env.step(int(a))
+        buffer.store(o_0, a, o_1, r, t)
+        o_0 = o_1.copy()
         if t:
             break
     env.close()
