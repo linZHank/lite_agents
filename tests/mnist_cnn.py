6864585951c33a2890c752c34085adbb681031e3
linzhank <linzhank@gmail.com>
2025-07-05 10:03:57 -0500

    update cnn test for mnist

--- tests/mnist_cnn.py
+++ tests/mnist_cnn.py
@@ -7,6 +7,10 @@ from torchvision import datasets
 from torchvision.transforms import v2
 
 import jax.numpy as jnp
+from flax import nnx  # The Flax NNX API.
+from functools import partial
+
+import matplotlib.pyplot as plt
 
 # Hyper-parameters
 BATCH_SIZE = 64
@@ -52,4 +56,29 @@ print(jsx.shape)  # 64x1x28x28
 print(jsy.shape)  # 64
 print(jsy)
 
+# Create a CNN
 
+
+class CNN(nnx.Module):
+    """A simple CNN model."""
+
+    def __init__(self, *, rngs: nnx.Rngs):
+        self.conv1 = nnx.Conv(1, 32, kernel_size=(3, 3), rngs=rngs)
+        self.conv2 = nnx.Conv(32, 64, kernel_size=(3, 3), rngs=rngs)
+        self.avg_pool = partial(nnx.avg_pool, window_shape=(2, 2), strides=(2, 2))
+        self.linear1 = nnx.Linear(3136, 256, rngs=rngs)
+        self.linear2 = nnx.Linear(256, 10, rngs=rngs)
+
+    def __call__(self, x):
+        x = self.avg_pool(nnx.relu(self.conv1(x)))
+        x = self.avg_pool(nnx.relu(self.conv2(x)))
+        x = x.reshape(x.shape[0], -1)  # flatten
+        x = nnx.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+
+
+# Instantiate the model.
+model = CNN(rngs=nnx.Rngs(0))
+# Visualize it.
+nnx.display(model)
