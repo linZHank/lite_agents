4d4fdd170fd615ff4e580555269210ccf1f77ac3
linzhank <linzhank@gmail.com>
2025-07-06 21:56:52 -0500

    start to break down official mnist example

--- tests/mnist_cnn.py
+++ tests/mnist_cnn.py
@@ -1,4 +1,5 @@
 import pytest
+from functools import partial
 
 from pathlib import Path
 import torch
@@ -9,7 +10,7 @@ from torchvision.transforms import v2
 from jax import random
 import jax.numpy as jnp
 from flax import nnx  # The Flax NNX API.
-from functools import partial
+import optax
 
 import matplotlib.pyplot as plt
 
@@ -60,7 +61,7 @@ print(jsy)
 # Create a CNN
 
 
-class CNN(nnx.Module):
+class VanillaConvNet(nnx.Module):
     """A simple CNN model."""
 
     def __init__(self, *, rngs: nnx.Rngs):
@@ -80,11 +81,98 @@ class CNN(nnx.Module):
 
 
 # Instantiate the model.
-model = CNN(rngs=nnx.Rngs(0))
+model = VanillaConvNet(rngs=nnx.Rngs(0))
 # Visualize it.
 nnx.display(model)
 # Sanity test
 dummy_x = random.uniform(key=random.key(0), shape=(3, 28, 28, 1))
-print(dummy_x.shape)
+# print(dummy_x.shape)
 dummy_preds = model(dummy_x)
-print(dummy_preds.shape)
+print(dummy_preds)
+
+# Create optimizer
+
+optimizer = nnx.Optimizer(model, optax.adamw(LEARNING_RATE, MOMENTUM))
+metrics = nnx.MultiMetric(
+    accuracy=nnx.metrics.Accuracy(),
+    loss=nnx.metrics.Average("loss"),
+)
+
+nnx.display(optimizer)
+
+
+# Define training steps
+def loss_fn(model: VanillaConvNet, batch):
+    logits = model(torch.permute(batch[0], (0, 2, 3, 1)).numpy())
+    loss = optax.softmax_cross_entropy_with_integer_labels(
+        logits=logits, labels=batch[1].numpy()
+    ).mean()
+    return loss, logits
+
+
+#
+#
+# @nnx.jit
+# def train_step(
+#     model: VanillaConvNet, optimizer: nnx.Optimizer, metrics: nnx.MultiMetric, batch
+# ):
+#     """Train for a single step."""
+#     grad_fn = nnx.value_and_grad(loss_fn, has_aux=True)
+#     (loss, logits), grads = grad_fn(model, batch)
+#     metrics.update(loss=loss, logits=logits, labels=batch["label"])  # In-place updates.
+#     optimizer.update(grads)  # In-place updates.
+#
+#
+# @nnx.jit
+# def eval_step(
+#     model: VanillaConvNet, metrics: nnx.MultiMetric, images_batch, labels_batch
+# ):
+#     loss, logits = loss_fn(model, images_batch, labels_batch)
+#     metrics.update(loss=loss, logits=logits, labels=labels_batch)  # In-place updates.
+#
+#
+# # Train model
+# metrics_history = {
+#     "train_loss": [],
+#     "train_accuracy": [],
+#     "test_loss": [],
+#     "test_accuracy": [],
+# }
+#
+# for step, batch in enumerate(loader_train):
+#     # Run the optimization for one step and make a stateful update to the following:
+#     # - The train state's model parameters
+#     # - The optimizer state
+#     # - The training loss and accuracy batch metrics
+#     train_step(model, optimizer, metrics, batch)
+#
+#     if step > 0 and (
+#         step % eval_every == 0 or step == train_steps - 1
+#     ):  # One training epoch has passed.
+#         # Log the training metrics.
+#         for metric, value in metrics.compute().items():  # Compute the metrics.
+#             metrics_history[f"train_{metric}"].append(value)  # Record the metrics.
+#         metrics.reset()  # Reset the metrics for the test set.
+#
+#         # Compute the metrics on the test set after each training epoch.
+#         for test_batch in test_ds.as_numpy_iterator():
+#             eval_step(model, metrics, test_batch)
+#
+#         # Log the test metrics.
+#         for metric, value in metrics.compute().items():
+#             metrics_history[f"test_{metric}"].append(value)
+#         metrics.reset()  # Reset the metrics for the next training epoch.
+#
+#         clear_output(wait=True)
+#         # Plot loss and accuracy in subplots
+#         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+#         ax1.set_title("Loss")
+#         ax2.set_title("Accuracy")
+#         for dataset in ("train", "test"):
+#             ax1.plot(metrics_history[f"{dataset}_loss"], label=f"{dataset}_loss")
+#             ax2.plot(
+#                 metrics_history[f"{dataset}_accuracy"], label=f"{dataset}_accuracy"
+#             )
+#         ax1.legend()
+#         ax2.legend()
+#         plt.show()
