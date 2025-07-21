832eca6e946f7037a6f6a59da3e7209716079d2f
linzhank <linzhank@gmali.com>
2025-07-21 13:33:26 -0500

    prototype MLPNet

--- src/lite_agents/vpg/constructor.py
+++ src/lite_agents/vpg/constructor.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
+import numpy as np
 
 import jax.numpy as jnp
+from flax import nnx
 
 from scipy.signal import lfilter
 
@@ -31,3 +33,57 @@ class VPGBuffer(ReplayBuffer):
         )
 
         return experience_batch
+
+
+class MLPNet(nnx.Module):
+    """A simple fully-connected Neural Network model"""
+
+    def __init__(
+        self,
+        *,
+        rngs: nnx.Rngs,
+        input_dim: int,
+        output_dim: int,
+        hidden_sizes: tuple = (64, 64),
+    ):
+        assert len(hidden_sizes) >= 1
+        self.rngs = rngs
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.backbone_sizes = (input_dim, *hidden_sizes)
+        self.backbone_transforms = []
+
+    def __call__(self, x):
+        for i in range(len(self.backbone_sizes) - 1):
+            x = nnx.relu(
+                nnx.Linear(
+                    self.backbone_sizes[i], self.backbone_sizes[i + 1], rngs=self.rngs
+                )(x)
+            )
+        y = nnx.Linear(self.backbone_sizes[-1], self.output_dim, rngs=self.rngs)(x)
+        return y
+
+
+# class VPGAgent:
+#     def __init__(self, env_name: str, actor_type: str, seed: int = 25):
+#         self.rngs = nnx.Rngs(seed)
+#
+#     @nnx.jit
+#     def make_decision(self, obs: np.ndarray):
+#         log_probs = nnx.log_softmax(model(obs))  # policy: log(pi(a|s))
+#         distribution = tfp.distributions.Categorical(logits=log_probs)
+#         sampled_action = distribution.sample(seed=rngs)
+#         # log_prob_as = distribution.log_prob(sampled_action)
+#         return sampled_action, log_probs
+#
+if __name__ == "__main__":
+    import gymnasium as gym
+
+    env = gym.make("CartPole-v1", render_mode="human")
+    model = MLPNet(
+        rngs=nnx.Rngs(0),
+        input_dim=env.observation_space.shape[0],
+        output_dim=env.action_space.n,
+    )
+    dumo = env.observation_space.sample()
+    print(model(dumo))
