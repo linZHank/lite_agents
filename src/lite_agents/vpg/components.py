30011d6c38e0ef4b04e84d5ecf8abdf949440171
linzhank <linzhank@gmali.com>
2025-07-22 10:05:52 -0500

    clean up actor class

--- src/lite_agents/vpg/components.py
+++ src/lite_agents/vpg/components.py
@@ -36,7 +36,30 @@ class VPGBuffer(ReplayBuffer):
         return experience_batch
 
 
-class CategoricalActor(nnx.Module):
+class MLPNet(nnx.Module):
+    """A simple multi-layer perceptron network"""
+
+    def __init__(
+        self,
+        rngs: nnx.Rngs,
+        input_dims: int,
+        output_dims: int,
+        hidden_sizes: tuple = (64, 64),
+    ):
+        self.backbone_sizes = (input_dims, *hidden_sizes)
+        self.backbone_transforms = []  # TODO: functionize
+        for i in range(len(self.backbone_sizes) - 1):
+            self.backbone_transforms.append(
+                nnx.Linear(
+                    self.backbone_sizes[i], self.backbone_sizes[i + 1], rngs=rngs
+                )
+            )
+        self.output_transform = nnx.Linear(
+            self.backbone_sizes[-1], output_dims, rngs=rngs
+        )
+
+
+class CategoricalActor(MLPNet):
     """Actor for discrete actions space"""
 
     def __init__(
@@ -46,18 +69,11 @@ class CategoricalActor(nnx.Module):
         action_dims: int,
         hidden_sizes: tuple = (64, 64),
     ):
-        self.observation_dim = observation_dims
-        self.action_dim = action_dims
-        self.backbone_sizes = (observation_dims, *hidden_sizes)
-        self.backbone_transforms = []  # TODO: functionize
-        for i in range(len(self.backbone_sizes) - 1):
-            self.backbone_transforms.append(
-                nnx.Linear(
-                    self.backbone_sizes[i], self.backbone_sizes[i + 1], rngs=rngs
-                )
-            )
-        self.output_transform = nnx.Linear(
-            self.backbone_sizes[-1], action_dims, rngs=rngs
+        super().__init__(
+            rngs=rngs,
+            input_dims=observation_dims,
+            output_dims=action_dims,
+            hidden_sizes=hidden_sizes,
         )
 
     def __call__(self, x):
@@ -68,60 +84,3 @@ class CategoricalActor(nnx.Module):
         pi = Categorical(logits=log_prob)
 
         return log_prob, pi
-
-
-class VPGAgent:
-    def __init__(
-        self,
-        env_fn,
-        seed: int = 25,
-    ) -> None:
-        self.actor = CategoricalActor(
-            env_fn.observation_space.shape[0], env_fn.action_space.n, seed=seed
-        )
-        self.rngs = self.actor.rngs
-
-    def make_decision(self, observation: np.ndarray):
-        logp, pi = self.actor(observation)
-        action = pi.sample(seed=self.rngs)
-        # log_prob_a_givens = pi.log_prob(action)
-        return action, logp
-
-    @nnx.jit
-    def compute_objective(self, experience_batch: ExperienceBatch):
-        _, policy_batch = self.actor(experience_batch.obs)
-        logpa_batch = policy_batch.log_prob(experience_batch.act)
-        objective_batch = experience_batch.ret * logpa_batch  # NOT expected return
-
-        return -objective_batch.mean()
-
-
-if __name__ == "__main__":
-    import gymnasium as gym
-
-    buffer = VPGBuffer([], [], [], [])
-    len_episode = 0
-
-    env = gym.make("CartPole-v1", render_mode="rbg_array")
-    agent = VPGAgent(env)
-    last_obs, info = env.reset()
-    for _ in range(env.spec.max_episode_steps):
-        act, logp = agent.make_decision(last_obs)
-        next_obs, rew, term, trunc, info = env.step(np.array(act))
-        print("\n")
-        print(f"last observation: {last_obs}")
-        print(f"action: {act}")
-        print(f"next observation: {next_obs}")
-        print(f"reward: {rew}")
-        print(f"episode terminated: {term}")
-        print(f"episode truncated: {trunc}")
-        print(f"info: {info}")
-        print("\n")
-        buffer.store_step(last_obs, act, rew)
-        len_episode += 1
-        last_obs = next_obs.copy()
-        if term or trunc:
-            last_obs, info = env.reset()
-
-    exp_bat = buffer.extract_experience()
-    obj_val = agent.compute_objective(exp_bat)
