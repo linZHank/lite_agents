a661ab4f1fe3d86b06558062a530678a613f0fd5
linzhank <linzhank@gmail.com>
2025-08-03 22:52:26 -0500

    add qnet class

--- tests/cartpole/dqn.py
+++ tests/cartpole/dqn.py
@@ -1,12 +1,34 @@
 import gymnasium as gym
 
+from flax import nnx
+
 
 # SETUP
-def resolve_and_assess(rngs, explore_epsilon, obs)
+class QValueNet(nnx.Module):
+    """MLP Critic"""
+
+    def __init__(self, rngs: nnx.Rngs):
+        self.linear1 = nnx.Linear(4, 64, rngs=rngs)
+        self.linear2 = nnx.Linear(64, 64, rngs=rngs)
+        self.linear3 = nnx.Linear(64, 2, rngs=rngs)
+
+    def __call__(self, x):
+        x = nnx.relu(self.linear1(x))
+        x = nnx.relu(self.linear2(x))
+        q = self.linear3(x)
+
+        return q
+
+
+def resolve_and_assess(rngs, critic, explore_epsilon, obs):
     # pred_act, q_val = resolve_and_assess(rngs, explore_epsilon, critic, last_obs)
-    q_val = critic(obs)
+    q_value = critic(obs)
+    greedy_action = q_value.argmax(axis=-1)
+    #
+    return q_value, greedy_action
 
 
+rngs = nnx.Rngs(25)
 journal_learn = {
     "episode_idx": 0,
     "step_idx": 0,
@@ -33,8 +55,7 @@ for st in range(1000 * env.spec.max_episode_steps):
     if term or trunc:
         journal_learn["episode_idx"] += 1
         journal_learn["averaged_return"].append(
-            sum(journal_learn["deposit_return"])
-            / len(journal_learn["deposit_return"])
+            sum(journal_learn["deposit_return"]) / len(journal_learn["deposit_return"])
         )
         # TODO: need a logger
         print(
