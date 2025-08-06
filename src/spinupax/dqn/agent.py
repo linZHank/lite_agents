a4a17f31ab547522048d59f31309967be9e24ad2
linzhank <linzhank@gmail.com>
2025-08-06 14:46:02 -0500

    start building dqn agent.learn()

--- src/spinupax/dqn/agent.py
+++ src/spinupax/dqn/agent.py
@@ -15,3 +15,23 @@ from spinupax.dqn.components import (
 )
 
 import matplotlib.pyplot as plt
+
+
+def learn(
+    env_name: str = "CartPole-v1",
+    env_options: Optional[dict] = {"render_mode": "rgb_array"},
+    seed: int = 25,
+    discount: float = 0.99,
+    max_steps=int(1e5),
+    warmup_episodes: int = 10,
+    learning_rate: float = 3e-4,
+    hidden_sizes: tuple = (64, 64),
+    epsilon_decay_episodes: int = 100,
+    experience_sample_size: int = 512,
+    eval_flag: bool = False,
+    ckpt_dir: PosixPath = Path("/tmp/spinupax/cartpole/dqn/checkpoints/"),
+    save_per_epoch: int = 10,
+):
+    # SETUP
+    env = gym.make(env_name, **env_options)
+    rngs = nnx.Rngs(seed)
